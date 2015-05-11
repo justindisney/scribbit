ca68021d448850c838b9df572cc0bc465a54f6d8
Justin Disney <justindisney@users.noreply.github.com>
2015-05-11 14:07:25 -0700

    add the x-editable lib to make it possible to do inline editing of a scribbit's name

--- public/js/events.js
+++ public/js/events.js
@@ -1,14 +1,24 @@
-$(document).ready(function() {
-    $("li.scribbit .delete").click(function() {
+$(document).ready(function () {
+    $.fn.editable.defaults.mode = 'inline';
+    $.fn.editable.defaults.ajaxOptions = {type: "PUT"};
+
+    $('li.scribbit h3 a').editable({
+        type: 'text',
+        pk: 1,
+        url: '/scribbit',
+        title: 'Enter username'
+    });
+
+    $("li.scribbit .delete").click(function () {
         var scribbit = $(this).parents("li").data("scribbit");
-        
+
         $.ajax({
             type: 'DELETE',
-            url: '/scribbit/' + scribbit + '?XDEBUG_SESSION_START=netbeans-xdebug',
-            success: function(data, textStatus, jqXHR) {
-                console.log(scribbit + ' deleted successfully');
+            url: '/scribbit/' + scribbit,
+            success: function (data, textStatus, jqXHR) {
+                window.location.assign("/");
             },
-            error: function(jqXHR, textStatus, errorThrown) {
+            error: function (jqXHR, textStatus, errorThrown) {
                 console.log(scribbit + ' delete failed');
             }
         });
