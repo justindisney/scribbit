153efe5e3a5803b4350ad6633e8c95bf6874c9b1
Justin Disney <justindisney@users.noreply.github.com>
2015-05-15 16:52:35 -0700

    add the directoy permissions updates to the install script

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
 if command -v composer 2>/dev/null; then
-    echo "we have composer"
+    echo "composer installed"
+    composer update
 else
     echo "composer not installed; downloading composer.phar"
-#    curl -sS https://getcomposer.org/installer | php -d allow_url_fopen=On
-#    php -d allow_url_fopen=On composer.phar update
+    curl -sS https://getcomposer.org/installer | php -d allow_url_fopen=On
+    php -d allow_url_fopen=On composer.phar update
 fi
+
+chmod -R 0777 scribbits/
+chmod -R 777 templates/
+chmod -R g+rwxs templates/cache/
