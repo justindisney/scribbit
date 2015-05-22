3168c7f44ff90887e11d5a26261450b0cf73c7a0
Justin Disney <justindisney@users.noreply.github.com>
2015-05-22 16:05:02 -0700

    now that there are 'require-dev' dependencies, tell composer not to install them in the install.sh script

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 
 if command -v composer 2>/dev/null; then
     echo "composer installed"
-    composer update
+    composer update --no-dev
 else
     echo "composer not installed; downloading composer.phar"
     curl -sS https://getcomposer.org/installer | php -d allow_url_fopen=On
-    php -d allow_url_fopen=On composer.phar update
+    php -d allow_url_fopen=On composer.phar update --no-dev
 fi
 
 chmod -R 0777 scribbits/
